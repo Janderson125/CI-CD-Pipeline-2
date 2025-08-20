ab2d6d0dcdfad1c042e594cf4302d9d9a2bacb78
Justin Anderson <jandersonperpetualhomes@gmail.com>
2025-08-20 15:10:06 -0500

    Initial commit: working React e-commerce app

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"]
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          browsers: ">0.25%, not dead", // adjust browser support as needed
+        },
+        useBuiltIns: "usage",
+        corejs: 3,
+      },
+    ],
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic", // no need to import React in every file
+      },
+    ],
+    "@babel/preset-typescript", // include if using TypeScript
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties", // optional, for class properties
+    "@babel/plugin-transform-runtime" // optional, reduces code duplication
+  ],
 };
